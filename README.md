b73d96713a1433e90834b6f1b24769867531d8b8
02485517 <nevincansel.efendioglu@kocsistem.com.tr>
2022-03-13 22:26:56 +0300

    finalizing extension

--- README.md
+++ README.md
@@ -8,5 +8,5 @@ The aim of the Mirror Repositories task is to copy of changes from one Git Repos
 The task uses basic git commands to mirror repositories given. But it runs on only **Windows** for now.
 Do not forget to give necessary permissions to repository. You can see details of configuration in the [article][medium-link].  
 
-[medium-link]: https://medium.com/p/1376bd5cdfb2
+[medium-link]: https://medium.com/@nevincansel/mirroring-git-repositories-1376bd5cdfb2
 [logo-image]: https://raw.githubusercontent.com/nevincansel/azuredevops-mirror-repositories/main/images/icon.png
--- mirrorRepos/mirror-repos.ps1
+++ mirrorRepos/mirror-repos.ps1
@@ -1,15 +1,20 @@
 [CmdletBinding()]
-param()
+
+param(
+    $env:AGENT_OS
+)
 
 Import-VstsLocStrings "$PSScriptRoot\task.json"
+
+if (!($env:AGENT_OS -like "*Windows*")){
+    Write-Error "This extension only runs on Windows but your OS is $env:AGENT_OS"
+}
 
 $sourceType = Get-VstsInput -Name "sourceType"
 $sourceURL = Get-VstsInput -Name "sourceURL"
 $sourcePAT = Get-VstsInput -Name "sourcePAT"
 $sourceUsername = Get-VstsInput -Name "sourceUsername"
 $sourcePass = Get-VstsInput -Name "sourcePass"
-
-
 if ($sourceType -eq "true"){
     Write-Host "Source is Cloud"
     $sourceURLHelper = "//"+$sourcePAT+"@"
@@ -20,61 +25,40 @@ else{
     $sourceURLHelper = "//"+$sourceUsername+":"+$sourcePass+"@"
     $sourceURL= $sourceURL -replace "//", $sourceURLHelper
 }
-
 Write-Host Starting the synchronization process
 git clone --mirror $sourceURL
-
 if ($sourceURL -like "*.git*"){
     $destinationPath = $sourceURL.Remove(0, $sourceURL.LastIndexOf('/') + 1)
 }
 else{
     $destinationPath = $sourceURL.Remove(0, $sourceURL.LastIndexOf('/') + 1) + ".git"
 }
-
 Set-Location $destinationPath
-
 $destinationType = Get-VstsInput -Name "destinationType"
 $destinationURL = Get-VstsInput -Name "destinationURL"
 $destinationPAT = Get-VstsInput -Name "destinationPAT"
-
-
-try {
-
 if ($destinationType -eq "true"){
     Write-Host "Destination is Cloud"
     $destinationURLHelper = "//"+$destinationPAT+"@"
     $destinationURL= $destinationURL -replace "//", $destinationURLHelper
-
     Write-Output "*****Git remote set destination URL****"
     git remote set-url origin $destinationURL
-
     Write-Output "*****Git fetch source repo****"
     git fetch $sourceURL
-
     Write-Output "*****Git push to destination repo****"
     git push -f
-
     Set-Location ..
     Remove-Item -Path $destinationPath -Force -Recurse 
 }
 else{
     Write-Host "Destination is not Cloud"
-
     $destinationPAT = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($destinationPAT)"))
     Write-Output "*****Git remote set destination URL****"
     git remote set-url origin $destinationURL
-
     Write-Output "*****Git fetch source repo****"
     git fetch $sourceURL
-
     Write-Output "*****Git push to destination repo****"
     git -c http.extraheader="AUTHORIZATION: basic $destinationPAT" push -f
-
     Set-Location ..
     Remove-Item -Path $destinationPath -Force -Recurse 
-}
-}
-catch [Exception] {    
-    Write-Error ($_.Exception.Message)
-    Throw
 }
